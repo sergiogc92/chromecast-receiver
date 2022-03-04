f12e6cf3c6ee0a5b6c660d4ef56a2ba404d5181d
sergiogc92 <sergiog.c.92@gmail.com>
2022-03-04 12:16:12 +0100

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,5 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 let playerElement = document.getElementsByTagName("cast-media-player")[0];
-playerElement.style.setProperty('--splash-image', 'url("logo.png")');
+playerElement.style.setProperty('--splash-image', 'url("https://github.com/sergiogc92/chromecast-receiver/blob/main/logo.png")');
 
 context.start();
