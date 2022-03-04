72a5bce34879b785acaf744f1335518609bbe051
sergiogc92 <sergiog.c.92@gmail.com>
2022-03-04 12:14:03 +0100

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -1,4 +1,5 @@
 const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
+let playerElement = document.getElementsByTagName("cast-media-player")[0];
+playerElement.style.setProperty('--splash-image', 'url("logo.png")');
 
 context.start();
